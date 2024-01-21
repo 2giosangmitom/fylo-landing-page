dc106697bb7a10862c9ddf413722b8129035902c
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-21 08:20:43 +0700

    feat: auto import scss variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,13 @@ import vue from "@astrojs/vue";
 
 export default defineConfig({
 	integrations: [vue()],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: "@import './src/assets/styles/variables.scss';",
+				},
+			},
+		},
+	},
 });
-
